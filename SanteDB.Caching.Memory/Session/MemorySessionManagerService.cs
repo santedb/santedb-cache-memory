1c4a928addf4db914b4023807c509b077d4a1f3a
Justin Fyfe <justin@fyfesoftware.ca>
2026-02-12 16:03:13 -0500

    Updated to allow local policy to augmentmentation to central policy

--- SanteDB.Caching.Memory/Session/MemorySessionManagerService.cs
+++ SanteDB.Caching.Memory/Session/MemorySessionManagerService.cs
@@ -168,9 +168,12 @@ namespace SanteDB.Caching.Memory.Session
                 else if(scope == null || scope.Contains("*")) // Validate the local environment doesn't have any other restrictions that the upstream granted
                 {
                     claims.RemoveAll(o => o.Type == SanteDBClaimTypes.SanteDBScopeClaim && !this.m_pepService.SoftDemand(o.Value, principal));
-                    
-                    //// Allow the local policy decision service to amend our list 
-                    //claims.AddRange(this.m_pdpService.GetEffectivePolicySet(principal).Where(o => o.Rule == Core.Model.Security.PolicyGrantType.Grant && !tokenPrincipal.HasClaim(p=>p.Type == SanteDBClaimTypes.SanteDBGrantedPolicyClaim && p.Value == o.Policy.Oid)).Select(c => new SanteDBClaim(SanteDBClaimTypes.SanteDBScopeClaim, c.Policy.Oid)));
+
+                    if (this.m_pdpService is IPolicyDecisionServiceEx pdpex)
+                    {
+                        // Allow the local policy decision service to amend our list 
+                        claims.AddRange(pdpex.ExpandInferredPolicies(claims.Where(o=>o.Type == SanteDBClaimTypes.SanteDBGrantedPolicyClaim).Select(o=>o.Value).ToArray()).Select(c => new SanteDBClaim(SanteDBClaimTypes.SanteDBScopeClaim, c)));
+                    }
                 }
 
                 if (scope?.Any(s => !s.Equals("*")) == true) // Demand additional scopes - ones that aren't on the principal (override check)
